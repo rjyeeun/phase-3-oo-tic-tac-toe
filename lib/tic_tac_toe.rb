19d8ab4b383405d55305d935894d4a21c0edec89
Yeeun Jung <rjyeeun@gmail.com>
2023-02-01 14:13:50 -0500

    commit tree 9965e189c6d717c76b9fec0adf5fd024477dae54

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,13 @@
+class TicTacToe
+    WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [6,7,8], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+    def initialize
+        @board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+    end
+    
+    def display_board
+        puts "x"
+    end
+    def input_to_index(input)
+        input.to_i
+    end
+end
